c1d0e1b5148f586d8051b6c96c12ac231951ab9b
MatheusJRM <matheusjrmoura@gmail.com>
2025-03-12 19:53:14 -0300

    feat: generalizando função util na aplicação

---
+++ src/utils/image-upload.ts
@@ -0,0 +1,24 @@
+import { useCallback } from "react";
+import { Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export const handleUploadImage = async (): Promise<string | null> => {
+  const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  if (status !== "granted") {
+    Alert.alert(
+      "Permissão necessária",
+      "Precisamos acessar sua galeria para escolher uma imagem."
+    );
+    return null;
+  }
+
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ["images"],
+    quality: 1,
+  });
+
+  if (result.canceled) {
+    return null;
+  }
+  return result.assets[0].uri;
+};
